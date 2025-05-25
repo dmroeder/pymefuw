5a743bbb03f38e1257a913d4f909a7aab56ffbeb
aawilliams85 <adair.williams85@gmail.com>
2025-05-25 17:55:16 -0400

    Naming + initial test

---
+++ tests/fuw_test.py
@@ -0,0 +1,29 @@
+import os
+import unittest
+
+from pymefuw import MEFUW
+
+# Turn off sort so that tests run in line order
+unittest.TestLoader.sortTestMethodsUsing = None
+
+class fuw_tests(unittest.TestCase):
+    def setUp(self):
+        self.ip_address = '192.168.1.20'
+
+        BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+        FUW_FOLDER_PATH = os.path.join(BASE_PATH, 'fuw')
+        self.fuw_helper_path = os.path.join(FUW_FOLDER_PATH, 'FUWhelper6xX.dll')
+        self.fuw_image_path = os.path.join(FUW_FOLDER_PATH, 'SC.IMG')
+        pass
+
+    def test_fuwhelper_load(self):
+        print('')
+
+        fuw = MEFUW(self.ip_address)
+        fuw.upgrade(self.fuw_helper_path, self.fuw_image_path)
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
