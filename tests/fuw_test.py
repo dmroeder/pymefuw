23298a0c29fce81a7c84614b24fadffc9dd4727f
aawilliams85 <adair.williams85@gmail.com>
2025-05-26 07:50:51 -0400

    Adjusted paths for multiple version testing

--- tests/fuw_test.py
+++ tests/fuw_test.py
@@ -6,8 +6,13 @@ from pymefuw import MEFUW
 # Turn off sort so that tests run in line order
 unittest.TestLoader.sortTestMethodsUsing = None
 
+# Expected folder structure
+# <Project Path>\tests\firmware\Helper\FUWhelper6xX.dll
+# <Project Path>\tests\firmware\FUC\PVP6\<Version>\upgrade\SC.IMG
+
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 FUW_FOLDER_PATH = os.path.join(BASE_PATH, 'fuw')
+FIRMWARE_FOLDER_PATH = os.path.join(BASE_PATH, 'firmware')
 
 class fuw_tests(unittest.TestCase):
     def setUp(self):
@@ -16,15 +21,19 @@ class fuw_tests(unittest.TestCase):
 
     def test_pvp6_v11(self):
         print('')
-        self.fuw_helper_path = os.path.join(FUW_FOLDER_PATH, 'FUWhelper6xX.dll')
-        self.fuw_image_path = os.path.join(FUW_FOLDER_PATH, 'SC_PVP6_v11.IMG')
+        self.fuw_helper_path = os.path.join(FIRMWARE_FOLDER_PATH, 'Helper', 'v15', 'FUWhelper6xX.dll')
+        self.fuw_image_path = os.path.join(FIRMWARE_FOLDER_PATH, 'FUC', 'PVP6', 'ME_PVP6xX_11.00-20190915', 'upgrade', 'SC.IMG')
+        print(self.fuw_helper_path)
+        print(self.fuw_image_path)
         fuw = MEFUW(self.ip_address)
         fuw.upgrade(self.fuw_helper_path, self.fuw_image_path)
 
     def test_pvp6_v12(self):
         print('')
-        self.fuw_helper_path = os.path.join(FUW_FOLDER_PATH, 'FUWhelper6xX.dll')
-        self.fuw_image_path = os.path.join(FUW_FOLDER_PATH, 'SC_PVP6_v12.IMG')
+        self.fuw_helper_path = os.path.join(FIRMWARE_FOLDER_PATH, 'Helper', 'v15', 'FUWhelper6xX.dll')
+        self.fuw_image_path = os.path.join(FIRMWARE_FOLDER_PATH, 'FUC', 'PVP6', 'ME_PVP6xX_12.00-20200922', 'upgrade', 'SC.IMG')
+        print(self.fuw_helper_path)
+        print(self.fuw_image_path)
         fuw = MEFUW(self.ip_address)
         fuw.upgrade(self.fuw_helper_path, self.fuw_image_path)
 
