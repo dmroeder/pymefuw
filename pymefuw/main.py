3239d64505f7d632b4c03cc777ea998c04f04997
aawilliams85 <adair.williams85@gmail.com>
2025-05-26 13:38:00 -0400

    Increase timeout for firmware upgrade card.  Previous test with routed path went long.

--- pymefuw/main.py
+++ pymefuw/main.py
@@ -39,8 +39,6 @@ class MEFirmware(object):
 
     def upgrade(self, fuw_helper_path: str, fuw_image_path: str):
         with comms.Driver(self.comms_path, self.driver) as cip:
-            cip.timeout = 240
-
             # Validate device at this communications path is a terminal of known version.
             self.device = terminal.validation.get_terminal_info(cip)
             if not(terminal.validation.is_terminal_valid(self.device)):
@@ -85,7 +83,7 @@ class MEFirmware(object):
 
             # Download firmware upgrade card to terminal
             try:
-                cip.timeout = 240
+                cip.timeout = 480
                 fuw_image_file = types.MEFile('SC.IMG',
                                             True,
                                             True,
